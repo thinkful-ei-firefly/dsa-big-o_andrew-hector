182ae4eb93f4d8c6af31a291739d44564afec1b6
Hector Valles <hmvalles75@yahoo.com>
2019-10-01 08:00:36 -0700

    11

---
+++ 11.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// A -> C
+// A -> B
+// C -> B
+// A -> C
+// B -> A
+// B -> C
+// A -> C
+
+function TOH(num, source, dest, temp) {
+  if (num === 1) {
+    console.log(`Moving disc from ${source} to ${dest}`);
+    return;
+  } else {
+    TOH(num - 1, source, temp, dest);
+    TOH(1, source, dest, temp);
+    TOH(num - 1, temp, dest, source);
+  }
+}
+console.log(TOH(3, 'A', 'C', 'B'));
+
+// O(n ^ 2)
