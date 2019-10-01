9fdd3842c8224db9552f52df38f79a21d4e35e73
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-01 09:28:20 -0600

    12 - revString

--- 12.js
+++ 12.js
@@ -1,9 +1,25 @@
 const sheep = function (num) {
-    for (let i = 1; i <= num; i++){
+    for (let i = 1; i <= num; i++) {
         console.log(`${i}: Another sheep has jumped over the fence`)
     }
     console.log('0: All sheep have jumped over the fence')
 }
 //o(n)
 
-sheep(3)
+const powerCalculator = function (x, y) {
+    if (y < 0) {
+        return 'exponent should be >= 0'
+    }
+    else {
+        return (x ^ y)
+    }
+}
+//o(1)
+
+const revString = function (string) {
+    let str = string.split("")
+    str = str.reverse()
+    str = str.join("")
+    return str
+}
+//o(1)
