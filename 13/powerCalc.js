e2b6f712f8cd164296d7757763ed820f30964675
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-01 09:57:34 -0600

    13 power

--- 13/powerCalc.js
+++ 13/powerCalc.js
@@ -1,16 +1,21 @@
-'use strict';
-
-function powerCalculater(num, exp) {
-  if (exp <= 0) {
-    console.log('Exponent should be >= 0');
-    return;
+const powerCalculator = function (x, y) {
+  if (y < 0) {
+      return 'exponent should be >= 0'
   }
-  if (exp === 1) {
-    return num;
+  else {
+      const power = function (a, b) {
+          if (b <= 0) {
+              return 1
+          }
+          else {
+              return (a * power(a, (b-1)))
+          }
+      }
+      return (x * (power(x, (y-1))))
   }
-  return num * powerCalculater(num, exp - 1);
 }
 
-console.log(powerCalculater(10, 4));
+console.log(powerCalculator(10, 2))
+console.log(powerCalculator(10, -2))
 
-// O(exp) Linear
+//O(n) since it only runs one for each value of y
