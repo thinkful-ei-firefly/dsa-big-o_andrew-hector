79e0690b8f31c2dc00a97d971b931923daa3b8f6
Hector Valles <hmvalles75@yahoo.com>
2019-10-01 08:29:14 -0700

    b

--- 12.js
+++ 12.js
@@ -1,9 +1,9 @@
-const sheep = function (num) {
-    for (let i = 1; i <= num; i++){
-        console.log(`${i}: Another sheep has jumped over the fence`)
-    }
-    console.log('0: All sheep have jumped over the fence')
-}
+const sheep = function(num) {
+  for (let i = 1; i <= num; i++) {
+    console.log(`${i}: Another sheep has jumped over the fence`);
+  }
+  console.log('0: All sheep have jumped over the fence');
+};
 //o(n)
 
-sheep(3)
+sheep(3);
