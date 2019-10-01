b1614c05ede61750f05d077bb1dc7a23345d1995
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-01 08:27:15 -0600

    2

---
+++ 2.js
@@ -0,0 +1,10 @@
+function isEven(value) {
+    if (value % 2 == 0) {
+        return true;
+    }
+    else
+        return false;
+    }
+}
+
+//O(1) because no matter how big N is, it will always take the same amount of time to solve.
