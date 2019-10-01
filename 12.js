0bc23cb31c64a5fdfde523c9f52b8c4af3e06f3f
Hector Valles <hmvalles75@yahoo.com>
2019-10-01 08:48:18 -0700

    12.6

--- 12.js
+++ 12.js
@@ -45,3 +45,20 @@ function stringSplitter(str, sep) {
 }
 
 console.log(stringSplitter('qw/er/ty', '/'));
+// O(n) Linear
+
+function compute(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    if (i === 1) {
+      result.push(0);
+    } else if (i == 2) {
+      result.push(1);
+    } else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  return result;
+}
+
+// O(n) Linear
